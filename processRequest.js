ec8030da7196e41ff047b722be787e4652be8ee8
Vectorii <vectoriidesign@gmail.com>
2022-04-16 00:27:22 +0000

    add error for api request

--- processRequest.js
+++ processRequest.js
@@ -1,41 +1,39 @@
 const fetch = require("node-fetch");
 
+var response;
+var error;
+
 async function process(request) {
     let requestClass = request["class"];
     let requestFunction = request["function"];
     let requestData = request["data"];
-    let error = false;
 
-    let response = {"status":200,"data":[]};
+    response = {"status":200,"data":[]};
+    error = false;
 
     try {
         switch (requestClass) {
-            case 'user':
-                switch(requestFunction) {
-                    case 'getJoinTimestamp':
-                        // request parameters: [username]
-                        // response values: [join date timestamp]
-                        let username;
-                        let timestamp;
-
-                        username = requestData[0];
-                        
-                        url = "https://api.scratch.mit.edu/users/"+username;
-                        apiRequest = await get(url).catch(function(status) {
-                            response = {"status":status,"data":[]};
-                            error = true;
-                        });
-                        if (!error) {
-                            timestamp = apiRequest["history"]["joined"];
-                            response["data"].push(timestamp);
-                        }
-                        break;
-                    default:
-                        response = {"status":400,"data":[]};
+        case 'user':
+            switch(requestFunction) {
+            case 'getJoinTimestamp':
+                // request parameters: [username]
+                // response values: [timestamp of user join date]
+                let username = requestData[0];;
+                let timestamp;
+                
+                url = "https://api.scratch.mit.edu/users/"+username;
+                apiRequest = await get(url);
+                if (!error) {
+                    timestamp = apiRequest["history"]["joined"];
+                    response["data"].push(timestamp);
                 }
                 break;
             default:
                 response = {"status":400,"data":[]};
+            }
+            break;
+        default:
+            response = {"status":400,"data":[]};
         }
     } catch(err) { 
         console.log(err);
@@ -46,17 +44,22 @@ async function process(request) {
 }
 
 async function get(url) {
-    return new Promise((resolve, reject) => {
-        fetch(url).then(function(response) {
-            if (!response.ok) {
-                reject(response.status);
-            }
-            response.json().then(function(data) {
-                resolve(data);
+    return run().catch(function(status) {
+        response = {"status":status,"data":[]};
+        error = true;
+    })
+    function run() {
+        return new Promise((resolve, reject) => {
+            fetch(url).then(function(response) {
+                if (!response.ok) {
+                    reject(response.status);
+                }
+                response.json().then(function(data) {
+                    resolve(data);
+                })
             })
         })
-    })
+    }
 }
-
 
 module.exports = { process };
